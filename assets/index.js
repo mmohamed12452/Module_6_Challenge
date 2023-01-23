70dcdfe6c22038ec9ace0b3b2b8b2722cebadafa
moassamo <moassermo@gmail.com>
2023-01-23 09:06:21 -0600

    function to pick and add a city from search field

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+// Variable declaration for our project
+var cityListArray = [];
+var cityName;
+
+//function to pick up and display the city being searched.
+function displaySearchedCity(){
+    $("#cityInput").val("");
+    $("#cityList").empty();
+
+
+    for(i=0; i<cityList.length; i++)
+    {
+       var cityLink = $("<a>");
+       cityLink.addClass("list-group-item list-group-item-action list-group-item-primary city");
+       cityLink.attr("data-name", cityListArray[i]);
+       cityLink.text(cityListArray[i]);
+       $("#cityList").prepend(cityLink);
+    }
+
+}
